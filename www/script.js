ec38894f89c740a55c0940994f57ac9de6ee1b02
Dirk Legler <dirko@dirko.net>
2014-12-30 13:44:50 +0100

    removed mensa menu from php version

--- www/script.js
+++ www/script.js
@@ -5,7 +5,6 @@ var version = "0"
 
 var news = new Array();
 var ticker = new Array();
-var mensa = new Array();
 var currentNews = 0;
 var currentTicker = 0;
 // last update (text)
@@ -24,7 +23,6 @@ var rssreq = new XMLHttpRequest();
 var tickerreq = new XMLHttpRequest();
 var versionreq = new XMLHttpRequest();
 var ipreq = new XMLHttpRequest();
-var mensareq = new XMLHttpRequest();
 
 
 function updateTicker() {
@@ -41,29 +39,6 @@ function saveTicker(tickerjson) {
 	luTicker = new Date().getTime();
 	ticker = tickertext["ticker"];
 	showTicker();
-}
-
-function updateMensa() {
-	mensareq.open('GET', 'mensa.fsr?alte-mensa');
-	mensareq.onreadystatechange = function() {
-    			if(this.readyState!=4) return;
-    			if(this.status==200) saveMensa(this.responseText);
-    		};
-    mensareq.send(null);
-}
-
-function saveMensa(mensatext) {
-	mensa = unescape(mensatext).split('</table>', 6);
-	showMensa();
-}
-
-function showMensa() {
-	var date = new Date();
-	var day = (date.getDay()+6)%7;
-	if(date.getHours()>15) {
-		day = (day+1)%7;
-	}
-	document.getElementById("mensa").innerHTML = mensa[day]+"</table>";
 }
 
 function updateVersion() {
@@ -153,8 +128,6 @@ window.onload = function() {
 	updateRSS();
 	setInterval('updateTicker()', 600000);
 	updateTicker();
-	setInterval('updateMensa()', 3600000);
-	updateMensa();
 	setInterval('updateRSS()', 1800000);
 	setInterval('showRSS()', 20000);
 	setInterval('showTicker()', 15000);
