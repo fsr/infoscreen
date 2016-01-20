2ca970d62f20b41f9729d4d52e2875b3dddd0db4
h4llow3En <Doering.Felix@googlemail.com>
2016-01-20 10:27:58 +0100

    make ticker to single line string

--- www/utils/ticker.py
+++ www/utils/ticker.py
@@ -7,6 +7,15 @@ def postillon_ticker():
     Get ticker data from the Postillon.
     TODO: abstract get_from_api() function?
     '''
-    ret = urllib.request.urlopen('http://www.der-postillion.de/ticker/newsticker2.php')
+    ret = urllib.request.urlopen(
+        'http://www.der-postillion.de/ticker/newsticker2.php')
     ticker = json.loads(ret.read().decode("utf-8-sig"))
-    return ticker["tickers"]
+    tickerstring = ''
+
+    space = 6
+    for line in ticker['tickers']:
+        tickerstring += '+++{ticker}+++{space}'.format(
+            ticker=line['text'], space=' ' * space)
+
+    tickerstring = tickerstring[:-space]
+    return tickerstring
