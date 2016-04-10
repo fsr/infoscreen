eed6a26960faec5b38564fe123ee811793b60d63
Lars Engeln <mail@lars-engeln.de>
2016-04-10 19:58:00 +0200

    adds postillon-impl

--- frontend/assets/src/InfoScreen.js
+++ frontend/assets/src/InfoScreen.js
@@ -28,7 +28,12 @@ var InfoScreenManager = function () {
     self.newsRefreshTime = 2000; //600000;   // all 10min
     self.mealsRefreshTime = 6000000; // all 100min
     self.busRefreshTime = 60000; // evr min
-    self.postillionRefreshTime = 600000; // all 10min
+    self.postillonRefreshTime = 600000; // all 10min
+
+    self.postillonTicker = [];
+    self.postillonAmount = 3;
+    self.postillonDisplayedIDs = [];
+    self.postillonTickerTime = 1000;
 
     self.fadeTime = 600;
 
@@ -42,23 +47,26 @@ InfoScreenManager.prototype = {
         'use strict';
         var self = this;
 
-        self.request("news" + self.postfix, self.refreshNews);
+        self.request("news", self.refreshNews);
         setInterval(function () {
-            self.request("news" + self.postfix, self.refreshNews);
+            self.request("news", self.refreshNews);
         }, self.newsRefreshTime);
-        self.request("meals" + self.postfix, self.refreshMensa);
+        self.request("meals", self.refreshMensa);
         setInterval(function () {
-            self.request("meals" + self.postfix, self.refreshMensa);
+            self.request("meals", self.refreshMensa);
         }, self.mealsRefreshTime);
-        self.request("stops" + self.postfix, self.refreshBus);
+        self.request("stops", self.refreshBus);
         setInterval(function () {
-            self.request("stops" + self.postfix, self.refreshBus);
+            self.request("stops", self.refreshBus);
         }, self.busRefreshTime);
-        self.request("postillion" + self.postfix, self.refreshPostillion);
+        self.request("postillon", self.refreshPostillon);
         setInterval(function () {
-            self.request("postillion" + self.postfix, self.refreshPostillion);
-        }, self.postillionRefreshTime);
+            self.request("postillon", self.refreshPostillon);
+        }, self.postillonRefreshTime);
 
+        setInterval(function () {
+            self.updatePostillon();
+        }, self.postillonTickerTime);
     },
     request: function (ressource, callback) {
         'use strict';
@@ -66,41 +74,120 @@ InfoScreenManager.prototype = {
 
         $.ajax({
             type: "GET",
-            url: self.basepath + ressource
+            url: self.basepath + ressource + self.postfix
         }).done(function (e) {
-            callback(e, self);
+            if (e !== undefined) {
+                callback(e, self);
+            }
         }).fail(function (e) {
             console.log("Could not load " + ressource + " [" + e.error_description + "]");
-
         });
     },
-    refreshNews: function (response, self) {
+    replace: function (elem, html, self) {
         'use strict';
 
-        console.log(response, self);
-        if (response !== undefined) {
-            if ($("#news").html() !== response) {
-                $("#news").fadeOut(self.fadeTime, function () {
-                    $("#news").html(response);
-                    $("#news").fadeIn(self.fadeTime);
+        if (html !== undefined && elem !== undefined) {
+            if (elem.html() !== html) {
+                elem.fadeOut(self.fadeTime, function () {
+                    elem.html(html);
+                    elem.fadeIn(self.fadeTime);
                 });
             }
         }
     },
+    refreshNews: function (response, self) {
+        'use strict';
+        var elem = $("#news > article");
+
+        self.replace(elem, response, self);
+
+    },
     refreshMensa: function (response, self) {
         'use strict';
+        var r, n, html, elem = $("#mensa > article");
 
+        html = "";
+
+        for (r in response) {
+            if (response.hasOwnProperty(r)) {
+                html += "<p>" + r.name + "</p>";
+                html += "<p>" + r.price + "</p>";
+                html += "<p>";
+                for (n in r.notes) {
+                    if (r.notes.hasOwnProperty(n)) {
+                        switch (n) {
+                        case "knoblauch":
+                            html += "k ";
+                            break;
+                        case "vegetarisch":
+                            html += "v ";
+                            break;
+                        case "vegan":
+                            html += "v ";
+                            break;
+                        case "schwein":
+                            html += "s ";
+                            break;
+                        case "rind":
+                            html += "r ";
+                            break;
+                        case "alkohol":
+                            html += "a ";
+                            break;
+                        }
+                    }
+                }
+                html += "</p>";
+            }
+        }
     },
     refreshWeather: function (response, self) {
         'use strict';
 
     },
-    refreshPostillion: function (response, self) {
+    refreshPostillon: function (response, self) {
         'use strict';
+
+        self.postillonTicker = response;
 
     },
     refreshBus: function (response, self) {
         'use strict';
 
+    },
+    updatePostillon: function () {
+        'use strict';
+        var amount, nextID, html, self = this;
+
+        if (self.postillonTicker.length > 0) {
+            amount = Math.min(self.postillonAmount, self.postillonTicker.length);
+            if (amount === 0) {
+                return;
+            }
+            nextID = 0;
+            if (self.postillonDisplayedIDs.length !== 0) {
+                nextID = (self.postillonDisplayedIDs[self.postillonDisplayedIDs.length - 1] + 1) % self.postillonTicker.length;
+            }
+            if (self.postillonDisplayedIDs.length === amount) {
+                $("#postillon" + self.postillonDisplayedIDs[0]).slideUp(self.fadeTime, function () {
+                    $(this).remove();
+                });
+                self.postillonDisplayedIDs.shift();
+            }
+            self.postillonDisplayedIDs.push(nextID);
+
+            html = '<p id="postillon';
+            html += nextID;
+            html += '">';
+            html += self.postillonTicker[nextID];
+            html += "</p>";
+            html = $(html);
+            html.hide();
+            $("#misc > article").append(html);
+            html.slideDown(self.fadeTime);
+        }
+    },
+    updateTime: function () {
+        'use strict';
     }
 };
