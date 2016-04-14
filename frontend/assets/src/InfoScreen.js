7be67adda4ffa1c801d8976ac3740a7f0920da82
h4llow3En <mail@felixdoering.com>
2016-04-14 16:10:37 +0200

    fixing datatype, fixing links in font-awesome

--- frontend/assets/src/InfoScreen.js
+++ frontend/assets/src/InfoScreen.js
@@ -14,18 +14,18 @@ function InfoScreen() {
 }
 
 // loads new Apps
-var InfoScreenManager = function () {
+var InfoScreenManager = function() {
     'use strict';
     var self = this;
 
-    $(document).bind('contextmenu', function (e) {
+    $(document).bind('contextmenu', function(e) {
         e.preventDefault();
     });
 
-    self.basepath = "./"; //"127.0.0.1:5000/";
-    self.postfix = ".json"; //"";
+    self.basepath = "/"; //"127.0.0.1:5000/";
+    self.postfix = ""; //"";
 
-    self.newsRefreshTime = 600000;   // all 10min
+    self.newsRefreshTime = 600000; // all 10min
     self.mealsRefreshTime = 6000000; // all 100min
     self.busRefreshTime = 60000; // evr min
     self.postillonRefreshTime = 600000; // all 10min
@@ -43,77 +43,78 @@ var InfoScreenManager = function () {
 InfoScreenManager.prototype = {
     constructor: InfoScreen,
 
-    init: function () {
+    init: function() {
         'use strict';
         var self = this;
 
         self.request("news", self.refreshNews);
-        setInterval(function () {
+        setInterval(function() {
             self.request("news", self.refreshNews);
         }, self.newsRefreshTime);
         self.request("meals", self.refreshMensa);
-        setInterval(function () {
+        setInterval(function() {
             self.request("meals", self.refreshMensa);
         }, self.mealsRefreshTime);
         self.request("stops", self.refreshBus);
-        setInterval(function () {
+        setInterval(function() {
             self.request("stops", self.refreshBus);
         }, self.busRefreshTime);
         self.request("postillon", self.refreshPostillon);
-        setInterval(function () {
+        setInterval(function() {
             self.request("postillon", self.refreshPostillon);
         }, self.postillonRefreshTime);
 
-        setInterval(function () {
+        setInterval(function() {
             self.updatePostillon();
         }, self.postillonTickerTime);
     },
-    request: function (ressource, callback) {
+    request: function(ressource, callback) {
         'use strict';
         var self = this;
 
         $.ajax({
             type: "GET",
+            dataType: "json",
             url: self.basepath + ressource + self.postfix
-        }).done(function (e) {
+        }).done(function(e) {
             if (e !== undefined) {
                 callback(e, self);
             }
-        }).fail(function (e) {
+        }).fail(function(e) {
             console.log("Could not load " + ressource + " [" + e.error_description + "]");
         });
     },
-    replace: function (elem, html, self) {
+    replace: function(elem, html, self) {
         'use strict';
 
         if (html !== undefined && elem !== undefined) {
             if (elem.html() !== html) {
-                elem.fadeOut(self.fadeTime, function () {
+                elem.fadeOut(self.fadeTime, function() {
                     elem.html(html);
                     elem.fadeIn(self.fadeTime);
                 });
             }
         }
     },
-    replace_simple: function (elem, html, self) {
+    replace_simple: function(elem, html, self) {
         'use strict';
 
         if (html !== undefined && elem !== undefined) {
             if (elem.html() !== html) {
-                
-                    elem.html(html);
-                    
+
+                elem.html(html);
+
             }
         }
     },
-    refreshNews: function (response, self) {
+    refreshNews: function(response, self) {
         'use strict';
         var elem = $("#news > article");
 
         self.replace(elem, response, self);
 
     },
-    refreshMensa: function (response, self) {
+    refreshMensa: function(response, self) {
         'use strict';
         var r, n, html, elem = $("#mensa > article");
 
@@ -129,107 +130,107 @@ InfoScreenManager.prototype = {
                 for (n in response[r].notes) {
                     if (response[r].notes.hasOwnProperty(n)) {
                         switch (response[r].notes[n]) {
-                        case "knoblauch":
-                            html += "k ";
-                            break;
-                        case "vegetarisch":
-                            html += "v ";
-                            break;
-                        case "vegan":
-                            html += "v ";
-                            break;
-                        case "schwein":
-                            html += "s ";
-                            break;
-                        case "rind":
-                            html += "r ";
-                            break;
-                        case "alkohol":
-                            html += "a ";
-                            break;
+                            case "knoblauch":
+                                html += "k ";
+                                break;
+                            case "vegetarisch":
+                                html += "v ";
+                                break;
+                            case "vegan":
+                                html += "v ";
+                                break;
+                            case "schwein":
+                                html += "s ";
+                                break;
+                            case "rind":
+                                html += "r ";
+                                break;
+                            case "alkohol":
+                                html += "a ";
+                                break;
                         }
                     }
                 }
-               // html += "</p></div>";
+                // html += "</p></div>";
                 html += "</p>";
-                
+
             }
         }
         self.replace(elem, html, self);
     },
-    refreshWeather: function (response, self) {
+    refreshWeather: function(response, self) {
         'use strict';
 
     },
-    refreshPostillon: function (response, self) {
+    refreshPostillon: function(response, self) {
         'use strict';
         self.postillonTicker = response;
 
     },
-    refreshBus: function (response, self) {
+    refreshBus: function(response, self) {
         'use strict';
         var s, r, html, elem = $("#dvb");
 
         html = "";
-        
+
         html += "<article><header>Helmholtzstraße</header>";
         for (s in response.helmholtzstrasse) { //alle Daten für Helmholzstraße
-            
-                var direction;
-                switch (response.helmholtzstrasse[s].name) {
-                    case "Striesen":
-                        direction = "left";
-                        break;
-                    case "Löbtau Süd":
-                        direction = "right";
-                        break;
-                    case "Btf. Gruna":
-                        direction = "left";
-                        break;
-                }
-                html += "<p><strong>" + response.helmholtzstrasse[s].minutes + "</strong> min";
-                html += "<i class='fa fa-angle-" + direction + "'></i>";
-                html += "<span>" + response.helmholtzstrasse[s].number + "</span>" + response.helmholtzstrasse[s].name;
-                
-            
+
+            var direction;
+            switch (response.helmholtzstrasse[s].name) {
+                case "Striesen":
+                    direction = "left";
+                    break;
+                case "Löbtau Süd":
+                    direction = "right";
+                    break;
+                case "Btf. Gruna":
+                    direction = "left";
+                    break;
+            }
+            html += "<p><strong>" + response.helmholtzstrasse[s].minutes + "</strong> min";
+            html += "<i class='fa fa-angle-" + direction + "'></i>";
+            html += "<span>" + response.helmholtzstrasse[s].number + "</span>" + response.helmholtzstrasse[s].name;
+
+
         }
         html += "</article><article><header>Münchner Platz</header>";
-        for (s in response.muenchnerplatz) { 
-           
-                var direction;
-                switch (response.muenchnerplatz[s].name) {
-                    case "Coschütz":
-                        direction = "up";
-                        break;
-                    case "Wilder Mann":
-                        direction = "down";
-                        break;
-                    case "Btf Trachenberge":
-                        direction = "down";
-                        break;
-                    case "Plauen":
-                        direction = "up";
-                        break;
-                }
-                html += "<p><strong>" + response.muenchnerplatz[s].minutes + "</strong> min";
-                html += "<i class='fa fa-angle-" + direction + "'></i>";
-                html += "<span>" + response.muenchnerplatz[s].number + "</span>" + response.muenchnerplatz[s].name;
-                
-            
+        for (s in response.muenchnerplatz) {
+
+            var direction;
+            switch (response.muenchnerplatz[s].name) {
+                case "Coschütz":
+                    direction = "up";
+                    break;
+                case "Wilder Mann":
+                    direction = "down";
+                    break;
+                case "Btf Trachenberge":
+                    direction = "down";
+                    break;
+                case "Plauen":
+                    direction = "up";
+                    break;
+            }
+            html += "<p><strong>" + response.muenchnerplatz[s].minutes + "</strong> min";
+            html += "<i class='fa fa-angle-" + direction + "'></i>";
+            html += "<span>" + response.muenchnerplatz[s].number + "</span>" + response.muenchnerplatz[s].name;
+
+
         }
         html += "</article><article><header>Technische Universität</header>";
-        for (s in response.technischeuniversitaet) { 
-            
-                var direction;
-                html += "<p><strong>" + response.technischeuniversitaet[s].minutes + "</strong> min";
-                html += "<span>" + response.technischeuniversitaet[s].number + "</span>" + response.technischeuniversitaet[s].name;
-                
-            
+        for (s in response.technischeuniversitaet) {
+
+            var direction;
+            html += "<p><strong>" + response.technischeuniversitaet[s].minutes + "</strong> min";
+            html += "<span>" + response.technischeuniversitaet[s].number + "</span>" + response.technischeuniversitaet[s].name;
+
+
         }
         html += "</article>";
         self.replace_simple(elem, html, self);
     },
-    updatePostillon: function () {
+    updatePostillon: function() {
         'use strict';
         var amount, nextID, html, self = this;
 
@@ -243,7 +244,7 @@ InfoScreenManager.prototype = {
                 nextID = (self.postillonDisplayedIDs[self.postillonDisplayedIDs.length - 1] + 1) % self.postillonTicker.length;
             }
             if (self.postillonDisplayedIDs.length === amount) {
-                $("#postillon" + self.postillonDisplayedIDs[0]).slideUp(self.fadeTime, function () {
+                $("#postillon" + self.postillonDisplayedIDs[0]).slideUp(self.fadeTime, function() {
                     $(this).remove();
                 });
                 self.postillonDisplayedIDs.shift();
@@ -261,7 +262,7 @@ InfoScreenManager.prototype = {
             html.slideDown(self.fadeTime);
         }
     },
-    updateTime: function () {
+    updateTime: function() {
         'use strict';
     }
 };
