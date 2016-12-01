5aa8918dca7cc556876445a46b2ff093f58fa6d2
Felix Döring <mail@felixdoering.com>
2016-12-01 13:46:08 +0100

    Update cron.sh

--- deployment/cron.sh
+++ deployment/cron.sh
@@ -1,32 +1,10 @@
 cat <<__EOF__ | crontab
-# Edit this file to introduce tasks to be run by cron.
-#
-# Each task to run has to be defined through a single line
-# indicating with different fields when the task will be run
-# and what command to run for the task
-#
-# To define the time you can provide concrete values for
-# minute (m), hour (h), day of month (dom), month (mon),
-# and day of week (dow) or use '*' in these fields (for 'any').#
-# Notice that tasks will be started based on the cron's system
-# daemon's notion of time and timezones.
-#
-# Output of the crontab jobs (including errors) is sent through
-# email to the user the crontab file belongs to (unless redirected).
-#
-# For example, you can run a backup of all your user accounts
-# at 5 a.m every week with:
-# 0 5 * * 1 tar -zcf /var/backups/home.tgz /home/
-#
-# For more information see the manual pages of crontab(5) and cron(8)
-#
 # m h  dom mon dow   command
 0 0   * * *   /opt/vc/bin/tvservice -o &
 0 7   * * *   /opt/vc/bin/tvservice -p && timeout 1 startx &
 * *   * * *   /usr/local/bin/python3 /home/pi/buerostatus/raspi.py
-*/5 * * * *   /usr/local/bin/python3 /home/pi/stuff/printer3.py
 @reboot /etc/init.d/infoscreen start
-@reboot cd /home/pi/stuff && screen -dmS printerLcd /usr/local/bin/python3 lcd3.py
+@reboot screen -dmS printerStatus /usr/local/bin/python3 /home/pi/printer-monitor/pi/printerstatus.py
 
 # Reboot everyday at 0715 in the morning
 15 7  * * *   reboot
