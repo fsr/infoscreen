bb12a71a8e2b15abd9bb1286e577ed6782ded718
h4llow3En <mail@felixdoering.com>
2017-01-11 15:53:54 +0100

    Final changes to a working Infoscreen

--- deployment/cron.sh
+++ deployment/cron.sh
@@ -3,9 +3,5 @@ cat <<__EOF__ | crontab
 0 0   * * *   /opt/vc/bin/tvservice -o &
 0 7   * * *   /opt/vc/bin/tvservice -p && timeout 1 startx &
 * *   * * *   /usr/local/bin/python3 /home/pi/buerostatus/raspi.py
-@reboot /etc/init.d/infoscreen start
-@reboot screen -dmS printerStatus /usr/local/bin/python3 /home/pi/printer-monitor/pi/printerstatus.py
-
-# Reboot everyday at 0715 in the morning
-15 7  * * *   reboot
+@reboot /usr/bin/tmux -f /dev/null new-session -s "printerStatus" -d "/usr/local/bin/python3 /home/pi/printer-monitor/pi/printerstatus.py"
 __EOF__
